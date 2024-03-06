906fb65a30488ded97f2e44cc62f6239f4ede183
Florian Beck <info@florian-beck.de>
2024-03-06 17:03:38 +0100

    Fixed merge error

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -7,8 +7,9 @@ use embedded_canvas::CCanvas;
 use crate::peripherals::display::{ColorMode, DisplayCanvas};
 
 mod default_watchface;
+pub use default_watchface::DefaultWatchface;
 
-pub trait WatchFace<'a> {
+pub trait WatchFace {
     /// Create new watchface
     fn new() -> Self;
 
@@ -17,9 +18,21 @@ pub trait WatchFace<'a> {
 }
 
 /// State for the watch face
+#[derive(Clone, Copy, Debug)]
 pub struct WatchFaceState {
     pub time: NaiveDateTime,
     // pub ble: ,
     pub percent: u8,
     pub charging: bool,
+}
+
+impl Default for WatchFaceState {
+    /// Set default values
+    fn default() -> Self {
+        Self {
+            time: NaiveDateTime::UNIX_EPOCH,
+            percent: 0,
+            charging: false,
+        }
+    }
 }
